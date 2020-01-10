d32f04eefbc8001bfe260ac50eb9d0fabfea9514
marcelorocha876 <marcelorocha876.mr@gmail.com>
2020-01-10 23:23:28 +0000

    coisas

---
+++ app/ObjectivoEmpresarial.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ObjectivoEmpresarial extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'objectivos_empresariais';
+}
